ee1edeb2b74b3f37571b6a7e4cafb47d6b89b4fa
tgautier <tgautier@hotmail.fr>
2022-07-27 14:57:02 +0200

    p7_au27.07.2022_avec likes+admin+css_à_revoir

--- back_end/routes/posts.js
+++ back_end/routes/posts.js
@@ -9,12 +9,22 @@ const router = express.Router();
 
 router.get("/", auth, postCtrl.getAllPosts);
 
+router.get("/:id", auth, postCtrl.getOnePost);
+
 router.get("/lastactivitypost", auth, postCtrl.getLastActivityPost);
 
 router.put("/:id", auth, multer, postCtrl.modifyPost);
 
 router.post("/", auth, multer, postCtrl.createPost);
 
+router.post("/reaction", auth, postCtrl.postReaction);
+
+router.post("/comment", auth, postCtrl.postComment);
+
 router.delete("/:id", auth, commentCtrl.deleteAllComments, postCtrl.deletePost);
+
+router.delete("/comment/:id", auth, postCtrl.deleteComment);
+
+router.delete("/:id", auth, postCtrl.deletePost);
 
 module.exports = router;
